c806ef0561c76374a08c35e9bde5c6275b428c8a
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2023-12-20 14:01:39 +0200

    #283 Bug fixed, if user enter invalid data, he gets Response status 400 Bad request

--- service-api/src/main/java/greencity/dto/user/UserManagementViewDto.java
+++ service-api/src/main/java/greencity/dto/user/UserManagementViewDto.java
@@ -2,6 +2,8 @@ package greencity.dto.user;
 
 import lombok.*;
 
+import javax.validation.constraints.Pattern;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -9,10 +11,13 @@ import lombok.*;
 @EqualsAndHashCode
 @Builder
 public class UserManagementViewDto {
+
+    @Pattern(regexp = "\\d+", message = "ID should contain only digits")
     private String id;
     private String name;
     private String email;
     private String userCredo;
     private String role;
+    @Pattern(regexp = "\\d+", message = "User status should contain only digits")
     private String userStatus;
 }
