82627ef11a88397e47ac413076d4ec7187457f63
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2023-12-21 11:43:44 +0200

    #304 Bug fixed, add 400 response status if user enter invalid data

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -131,7 +131,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<Object> handleWrongIdException(WrongIdException ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
         log.trace(ex.getMessage(), ex);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
     /**
