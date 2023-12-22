f499086515c6cac7c16e14e20c175e3d25fa96f3
Kizerov Dmytro <kizerov.dmitriy@gmail.com>
2023-12-22 13:50:22 +0200

    Validate email

--- service-api/src/main/java/greencity/dto/user/UserManagementViewDto.java
+++ service-api/src/main/java/greencity/dto/user/UserManagementViewDto.java
@@ -2,6 +2,7 @@ package greencity.dto.user;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
 
 @AllArgsConstructor
@@ -11,10 +12,10 @@ import javax.validation.constraints.Pattern;
 @EqualsAndHashCode
 @Builder
 public class UserManagementViewDto {
-
     @Pattern(regexp = "\\d+", message = "ID should contain only digits")
     private String id;
     private String name;
+    @Email
     private String email;
     private String userCredo;
     private String role;
