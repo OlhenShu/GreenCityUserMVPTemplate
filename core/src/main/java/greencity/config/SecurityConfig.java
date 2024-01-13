0bd22102d0e823fcbcb09d23154d85c383fb79fc
Vlada <proskurina.vlada@gmail.com>
2024-01-13 14:26:32 +0200

    Fix

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package greencity.config;
 
 import greencity.security.filters.AccessTokenAuthenticationFilter;
+import greencity.security.handlers.CustomOAuth2AuthenticationSuccessHandler;
 import greencity.security.jwt.JwtTool;
 import greencity.security.providers.JwtAuthenticationProvider;
 import greencity.service.UserService;
@@ -32,6 +33,7 @@ import static greencity.constant.AppConstant.UBS_EMPLOYEE;
 import static greencity.constant.AppConstant.USER;
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 
 /**
  * Config for security.
@@ -92,12 +94,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/ownSecurity/verifyEmail",
                         "/ownSecurity/updateAccessToken",
                         "/ownSecurity/restorePassword",
+                        "/user/emailNotifications",
                         "/user/activatedUsersAmount",
                         "/user/{userId}/habit/assign",
                         "/token",
                         "/socket/**",
                         "/user/findAllByEmailNotification",
-                        "/user/checkByUuid")
+                        "/user/checkByUuid",
+                        "/login/**",
+                        "/oauth2/**")
                 .permitAll()
                 .antMatchers(HttpMethod.POST,
                         "/ownSecurity/signUp",
@@ -133,7 +138,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         USER_LINK,
                         "/user/shopping-list-items",
                         "/user/{userId}/habit",
-                        "/ownSecurity/set-password")
+                        "/email/sendHabitNotification",
+                        "/ownSecurity/set-password",
+                        "/ownSecurity/set-password",
+                        "/ownSecurity/set-password",
+                        "/email/sendUserViolation",
+                        "/email/changePlaceStatus")
                 .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
                 .antMatchers(HttpMethod.PUT,
                         "/ownSecurity/changePassword",
@@ -189,7 +199,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/css/**",
                         "/img/**")
                 .permitAll()
-                .anyRequest().hasAnyRole(ADMIN);
+                .anyRequest().hasAnyRole(ADMIN)            .and()
+                .oauth2Login()
+                .successHandler(new CustomOAuth2AuthenticationSuccessHandler(oauthservice))
+                .failureHandler((req, resp, exc) -> resp.sendError(SC_BAD_REQUEST, exc.getMessage()));
     }
 
     /**
